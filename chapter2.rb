989ffe33f02829c1860e1bc0ff490b209f8368e8
梁仁耀 <ryouhitoshiyao@ryouhitoshiyao-no-MacBook-Pro.local>
2018-12-16 18:44:36 +0900

    2.3

--- chapter2.rb
+++ chapter2.rb
@@ -21,8 +21,120 @@ p "  "
 
 p 10.to_s(
     16
-)
+    )
 
+p 10.to_s\
+16
+
+p "  "
+
+#command1
+
+=begin
+command2
+command3
+command4
+=end
+
+=begin
+予約語
+BEGIN
+class
+ensure
+nil
+self
+when
+END
+def
+false
+not
+super
+while
+alias
+defined?
+for
+or
+then
+yeild
+and
+do
+if
+redo
+true
+__LINE__
+begin
+else
+in
+rescue
+undef
+__FILE__
+break
+elsif
+module
+retry
+unless
+__ENCODING__
+case
+end
+next
+return
+until
+=end
+
+p "  "
+
+p (5 + 1 - 2)*3
+
+def add(a, b)
+    a + b
+end
+
+p add(4, 5)
+
+p "  "
+
+specila_price = 200
+
+a = b = 100
+
+p "  "
+
+puts "hellow\ntony"
+
+puts 'hellow\ntony'
+
+name = 'tony'
+puts "hellow,#{name}!"
+
+i = 10
+puts "#{i}は１６進数にすると#{i.to_s(16)}です"
+
+puts 'hellow,#{name}'
+
+puts 'hellow,' + name + '!'
+
+puts 'He said, "Don\'t speak."'
+puts "He said, \"Don't speak.\""
+
+p "  "
+
+p 'ruby' == 'ruby'
+p 'ruby' == 'Ruby'
+p 'ruby' != 'perl'
+p 'ruby' != 'ruby'
+
+p "  "
+
+p 'a' < 'b'
+p 'a' < 'A'
+p 'a' > 'A'
+
+p "  "
+
+p 'abc' < 'def'
+p 'abc' < 'ab'
+p 'abc' < 'abcd'
+p 'あいうえお' < 'かきくけこ'
 
 # p fizz_buzz(1)
 # p fizz_buzz(2)
